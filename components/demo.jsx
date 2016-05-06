85fe1ca7dc7ee95102c187bc0c15dfa16b5cadc3
jermaine anderson <jermaine.anderson@live.com>
2016-05-06 23:46:28 +0100

    application has is now using react to render its content

--- components/demo.jsx
+++ components/demo.jsx
@@ -8,41 +8,41 @@ var Demo = React.createClass({
 		          <p className="h3"> Please select which items you would like to add to your shopping bag</p>
 		        <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Isla Bra">
+		            <input type="checkbox" name="referrer" value="Isla Bra"/>
 		            Isla Bra (LN332) - £29
 		          </label>
 		        </p>
 		          <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Nordic Rose Bra">
+		            <input type="checkbox" name="referrer" value="Nordic Rose Bra"/>
 		            Nordic Rose Bra (LN336) - £30
 		          </label>
 		        </p>
 		        <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Zentangle Bra">
+		            <input type="checkbox" name="referrer" value="Zentangle Bra"/>
 		            Zentangle Bra (FY240) - £34
 		          </label>
 		        </p>
 		        <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Clara Bra">
+		            <input type="checkbox" name="referrer" value="Clara Bra"/>
 		            Clara Bra (PN112) - £32
 		          </label>
 		        </p>
 		        <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Deco Delight Bra">
+		            <input type="checkbox" name="referrer" value="Deco Delight Bra"/>
 		            Deco Delight Bra (FY158) - £34
 		          </label>
 		        </p>className
 		        <p className="checkbox">
 		          <label>
-		            <input type="checkbox" name="referrer" value="Sienna Bra">
+		            <input type="checkbox" name="referrer" value="Sienna Bra"/>
 		            Sienna Bra (LN328) - £32
 		          </label>
 		        </p>
-		        <p><input type="submit" value="Submit"></p>
+		        <p><input type="submit" value="Submit"/></p>
 		        </form>
 	      </div>
 		);
