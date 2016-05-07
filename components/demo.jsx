e9af7125b22fb927501b38da2853b0cae37db9d1
jermaine anderson <jermaine.anderson@live.com>
2016-05-07 11:59:34 +0100

    added some propTypes in order to resolve the warning message being displayed in the console

--- components/demo.jsx
+++ components/demo.jsx
@@ -1,6 +1,6 @@
 var Demo = React.createClass({
 	render: function() {
-
+		//list of products to be render in the DOM
 		var productOptions = [
 			{value: 'Isla Bra', label: 'Isla Bra (LN332) - £29'},
 			{value: 'Nordic Rose Bra', label: 'Nordic Rose Bra (LN336) - £30'},
@@ -27,11 +27,17 @@ var Demo = React.createClass({
 });
 
 var CheckBox = React.createClass({
+	// declaring property types
+	propTypes: {
+		value: React.PropTypes.string,
+		name: React.PropTypes.string,
+		children: React.PropTypes.node
+	},
 		render: function() {
 			return (
 				<p className="checkbox">
 			          <label>
-			            <input type="checkbox" name="referrer" value={this.props.value}/>
+			            <input type="checkbox" name={this.props.name} value={this.props.value}/>
 			            {this.props.children}
 			          </label>
 				</p>
@@ -41,12 +47,18 @@ var CheckBox = React.createClass({
 
 
 var ProductOptionsGroup = React.createClass({
+	propTypes: {
+		name: React.PropTypes.string
+	},
+
 	render: function() {
+		//obtaining the name of the properties so that it can be passed on
+		var name = this.props.name;
 		return (
 			<div>
 				{this.props.options.map(function (option) {
 					return (
-						<CheckBox value={option.value} key={option.value}>
+						<CheckBox name={name} value={option.value} key={option.value}>
 			              	{option.label}
 			            </CheckBox>
 					);
