5e23b48cd2d44ac023188ded839021877d9a05dc
jermaine anderson <jermaine.anderson@live.com>
2016-05-08 21:38:49 +0100

    creating get ajax request which should help with the post ajax request

--- public/js/main.js
+++ public/js/main.js
@@ -17,11 +17,46 @@ var Product = React.createClass({
 });
 
 
+var ProductBox = React.createClass({
+  getProductsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.getProductsFromServer();
+    setInterval(this.getProductsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="productBox">
+        <h1>Products</h1>
+        <ProductList data={this.state.data} />
+        <ProductForm  />
+      </div>
+    );
+  }
+});
+
+
 var ProductList = React.createClass({
   render: function() {
     var productThing = this.props.data.map(function(product) {
       return (
-  
+        <Product product_name={product.product_name} key={product.id}>
+          {product.code}
+        </Product>
       );
     });
     return (
