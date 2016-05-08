c1b6551deae8e0c9f9e0b8d19d1fb999c66b2e0e
jermaine anderson <jermaine.anderson@live.com>
2016-05-08 22:06:10 +0100

    main.js is almost complete

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 var port = process.env.PORT || 3000;
+var serverName = 'Bravisimmo App';
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
+app.locals.title = 'React Api'
 //test data. 
 var foundProduct = [
     {
         "id": 1462481527397,
-        "product_name": "Isla Bra",
+        "product_name": "Isla Bra - £29",
         "code": "(LN332)"
     },
     {
         "id": 1462481555490,
-        "product_name": "Nordic Rose Bra",
+        "product_name": "Nordic Rose Bra - £30'",
         "code": "(LN336)"
     },
     {
         "id": 1462640555180,
-        "product_name": "Zentangle Bra",
+        "product_name": "Zentangle Bra - £34",
         "code": "(FY240)"
     },
     {
         "id": 1462640693274,
-        "product_name": "Clara Bra",
+        "product_name": "Clara Bra - £32",
         "code": "(PN112)"
     },
     {
         "id": 1462640713835,
-        "product_name": "Deco Delight Bra",
+        "product_name": "Deco Delight Bra - £34",
         "code": "(FY158)"
     },
     {
         "id": 1462640723835,
-        "product_name": "Sienna Bra ",
+        "product_name": "Sienna Bra - £32",
         "code": "(LN328)"
     }
 ]
@@ -71,5 +74,5 @@ app.post('/api/bags', function(req, res) {
 
 
 app.listen(port, function() {
-  console.log('Server is now running on port ' + port);
+  console.log(serverName + ' Server is now running on port ' + port);
 });
